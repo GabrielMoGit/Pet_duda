437d25289bbafa435be3c6b73b8aa657b60d0c15
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-04-01 16:55:47 -0300

    Criado métoro create para poder cadastrar um usuário ao banco de dados

--- src/controller/tutorController.ts
+++ src/controller/tutorController.ts
@@ -1,8 +1,24 @@
 import { Request, Response} from "express"
+import { TutorRepository } from "../repositories/tutorRepository";
 
 class tutorController{
-
     async create(request: Request, response: Response){
+        const {name, phone} = request.body
+        const tutorRepository = new TutorRepository()
+     
+        const tutorAlreadyExist = await tutorRepository.findByPhone(phone)
+
+        if(tutorAlreadyExist){
+            return response.status(400).json({
+                error: "Telefone já cadastrado"
+            })
+        }
+
+        const tutor = tutorRepository.createAndSave(name, phone)
+        
+        return response.status(200).json({
+            message: "Tutor cadastrado!"
+        })
         
     }
 }
