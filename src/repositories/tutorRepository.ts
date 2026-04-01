7b3987020e2bf9ba206f30937e193b067f3f1f9f
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-04-01 16:41:14 -0300

    lincado o repositório ao modelo de tutor

--- src/repositories/tutorRepository.ts
+++ src/repositories/tutorRepository.ts
@@ -1,8 +1,14 @@
 import { dataSource } from "../database/dataSource";
+import { tutors } from "../models/tutor";
+import { Repository } from "typeorm";
 
 class tutorRepository{
 
-    
+    private repository: Repository<tutors>
+
+    constructor(){
+        this.repository = dataSource.getRepository(tutors)
+    }
 }
 
 export {tutorRepository}
