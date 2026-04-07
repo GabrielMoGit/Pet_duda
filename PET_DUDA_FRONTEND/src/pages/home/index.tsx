b7a22bfc24fb0348094c6ed2a51c8be3cb7de521
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-04-07 19:57:58 -0300

    Apenas exclusão de alguns arquivos e alteração de outros, e adicionado um botão que conta a quantidade de vezes que clicou, apenas para teste do react

--- PET_DUDA_FRONTEND/src/pages/home/index.tsx
+++ PET_DUDA_FRONTEND/src/pages/home/index.tsx
@@ -1,11 +1,16 @@
 import { Tittle } from './styles'
 import { MyButton } from '../../components/buttons/styles'
+import { useState} from 'react'
 
 function Home(){
 
+    const [count, setCount] = useState(0)
+
     return <div>
         <Tittle>Home</Tittle>
-        <MyButton>Clique em mim</MyButton>
+        <MyButton onClick={() => setCount(count + 1)}>
+            Clicou {count} vezes
+        </MyButton>
     </div>
 }
 
