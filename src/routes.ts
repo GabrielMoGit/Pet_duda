dcfd50559c99c8422ca8915354eea580c68379d8
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-04-09 20:15:51 -0300

    Criada a rota para poder criar o pet

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { tutorController} from "./controller/tutorController";
+import { petController } from "./controller/petController";
 
 const router = Router();
 
-const TutorController = new tutorController();
+const TutorController = new tutorController()
+const PetController = new petController()
 
 router.post("/tutor", TutorController.create)
+router.post("/pet", PetController.create)
 
 export {router};
