2ea2838fbaf41a93408a6125b076efe9a800f619
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-04-10 18:28:00 -0300

    Criada a migration, mas aguardandod para fazer as demais e rodas todas junto

---
+++ src/database/migrations/1775856193536-Streets.ts
@@ -0,0 +1,30 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class Streets1775856193536 implements MigrationInterface {
+    name = 'Streets1775856193536'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+                    new Table({
+                        name: "streets",
+                        columns:[
+                            {
+                                name: "id",
+                                type: "uuid",
+                                isPrimary: true
+                            },
+                            {
+                                name: "name",
+                                type: "varchar"
+                            },
+        
+                        ]
+                    })
+                )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("streets")
+    }
+
+}
