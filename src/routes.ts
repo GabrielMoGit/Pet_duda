d4d29fb9bad4eeeda8e40046f4a6572f95a1ade8
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-04-16 02:50:13 -0300

    Alteração insignificante

--- src/routes.ts
+++ src/routes.ts
@@ -2,16 +2,23 @@ import { Router } from "express";
 import { tutorController} from "./controller/tutorController";
 import { petController } from "./controller/petController";
 import { streetController } from "./controller/streetController";
+import { NeighborhoodController } from "./controller/neighberhoodController";
 
 const router = Router();
 
 const TutorController = new tutorController()
 const PetController = new petController()
 const StreetController = new streetController()
+const neighberhoodController = new NeighborhoodController()
 
 router.post("/tutor", TutorController.create)
+
 router.post("/pet", PetController.create)
+
 router.post("/street", StreetController.create)
 router.get("/listStreets", StreetController.listStreets)
+
+router.post("/neighborhood", neighberhoodController.create)
+router.get("listNeighborhood", neighberhoodController.listNeighborhoods)
 
 export {router};
