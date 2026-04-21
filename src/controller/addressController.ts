9b96fdc3aee6a860ab8c8f17ea33e50a1f478c4b
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-04-20 21:50:26 -0300

    Adicionada a rotina para criação de endereços com as informaçõs que serão passadas pelo usuário

--- src/controller/addressController.ts
+++ src/controller/addressController.ts
@@ -1,15 +1,34 @@
 import { Request, Response } from "express";
 import { AddressRepository } from "../repositories/addressRepository";
+import { StreetRepository } from "../repositories/streetReposiroty";
+import { NeighborhoodRepository } from "../repositories/neighborhoodRepository";
+import { TutorRepository } from "../repositories/tutorRepository";
 
 class AddressController{
 
     async create(request: Request, response: Response){
-        const {tutor_id, neighborhood_id, street_id, number} = request.body
+        const {tutorPhone, streetName, neighborhoodName, number} = request.body
         const addressRepository = new AddressRepository()
+        const streetReposiroty = new StreetRepository()
+        const neighborhoodRepository = new NeighborhoodRepository()
+        const tutorRepository = new TutorRepository()
+
 
         try{
-            const address = await addressRepository.createAndSave(tutor_id, neighborhood_id, street_id, number)
-            return response.json(address)
+
+            const street = await streetReposiroty.checkIfStreetAlreadyExist(streetName)
+            const neighborhood = await neighborhoodRepository.checkIfNeighborhoodAlreadyExist(neighborhoodName)
+            const tutor = await tutorRepository.findByPhone(tutorPhone)
+
+
+            const addressAlreadyExist = await addressRepository.checkIfAddressAlreadyExist(tutor!.id, neighborhood!.id, street!.id, number)
+
+            if(!addressAlreadyExist){
+                const address = await addressRepository.createAndSave(tutor!.id, neighborhood!.id, street!.id, number)
+                return response.json(address)
+            }
+
+            
         }catch(error){
             return response.status(500).json({
                 message: "Erro ao cadastrar endereço"
