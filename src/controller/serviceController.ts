83fbb93427a64da62b7e77ea8e2f1507ad0c00d4
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-04-28 10:58:08 -0300

    Criado os métodos de controller  de serviços para poder criar os serviços de acordo com o pacote necessário

---
+++ src/controller/serviceController.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from "express";
+import { ServiceRepository } from "../repositories/serviceRespository";
+import { ServiePackageRepository } from "../repositories/servicePackageRepository";
+import { INTERNAL } from "sqlite3";
+
+class ServiceController{
+
+    private createBiweeklyDates(initialDate: Date): Date[]{
+        const dates: Date[] = []
+
+        dates.push(new Date(initialDate))
+
+        const secondDate = new Date(initialDate)
+        secondDate.setDate(initialDate.getDate() + 15)
+        
+        dates.push(secondDate)
+
+        return dates
+    }
+
+    private createWeeklyDates(initialDate: Date): Date[]{
+        const dates: Date [] = []
+
+        dates.push(new Date(initialDate))
+
+        for(let i = 0; i < 3; i ++){
+            const newDate = new Date(initialDate)
+            newDate.setDate(initialDate.getDate() + ((i + 1) * 7))
+            dates.push(newDate)
+        }
+
+        return dates
+        
+    }
+
+    async createBiweekly(request: Request, response: Response){
+        const {service_package_id, service_date} = request.body
+
+        const serviceRepository = new ServiceRepository()
+
+        const dates = this.createBiweeklyDates(new Date(service_date))
+
+        try{
+
+            const results = []
+
+            for(let i = 0; i < dates.length; i ++){
+                const services = await serviceRepository.createAndSave(service_package_id, dates[i], false)
+                results.push(services)
+            }
+            
+            return response.json(results)
+
+        }catch(error){
+            return response.status(500).json({ error: "Erro ao criar serviços" })
+        }
+
+        
+    }
+
+    async createWeekly(request: Request, response: Response){
+
+    }
+
+}
+
+export { ServiceController }
---
+++ src/controller/servicePackageController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+import { ServiceRepository } from "../repositories/serviceRespository";
+import { ServiePackageRepository } from "../repositories/servicePackageRepository";
+
+class ServicePackageController{
+
+}
+
+export { ServicePackageController }
