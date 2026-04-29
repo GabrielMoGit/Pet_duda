caf854385a3e3baf1ac34dc48ff6c5a60e338a37
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-04-29 16:40:41 -0300

    Começo da criação da página que vai carregar os pacotes

--- PET_DUDA_FRONTEND/src/pages/Home/index.tsx
+++ PET_DUDA_FRONTEND/src/pages/Home/index.tsx
@@ -1,5 +1,13 @@
+import { useState, useEffect } from "react"
+import { api } from "../../services/api"
 
 export function Home(){
+
+    const [services, setServices] = useState([])
+
+    useEffect(() => {
+        
+    })
 
     return(
         <div>
