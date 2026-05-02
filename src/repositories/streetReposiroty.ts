08d12910f341559d21881958e5e6852ffced84ea
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-05-02 00:57:43 -0300

    Adicionado método de repositorio de ruas para localizar o nome da rua usando o id dela

--- src/repositories/streetReposiroty.ts
+++ src/repositories/streetReposiroty.ts
@@ -22,8 +22,13 @@ class StreetRepository {
     async listExistentStreetsOnlyStringName(){
         const streets =  await this.repository.find({
             select: ["name"]
-        })
+        })  
         return streets.map(streets => streets.name)
+    }
+
+    async returnStreetNameFromId(id: string){
+        const streets = await this.repository.findOneBy({id})
+        return(streets?.name)
     }
 }
 
