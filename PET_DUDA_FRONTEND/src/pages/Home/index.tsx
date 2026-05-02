2050fb811f4eec5528ff9411bace77eb48af7acf
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-05-01 21:43:54 -0300

    O frontend já consegue carregar os dados dos pacotes, agora é trabalhar pra refinar as informações a serem trazidas antes de refinar o frontend

--- PET_DUDA_FRONTEND/src/pages/Home/index.tsx
+++ PET_DUDA_FRONTEND/src/pages/Home/index.tsx
@@ -3,16 +3,39 @@ import { api } from "../../services/api"
 
 export function Home(){
 
-    const [services, setServices] = useState([])
+    type ServicePackage = {
+        id: number;
+        pet_id: number;
+        package_type: string;
+        package_done: number;
+        package_paid: number
+    }
+
+    const [servicePackage, setServicePackage] = useState<ServicePackage[]>([])
 
     useEffect(() => {
         
-    })
+        async function listOnload(){
 
-    return(
-        <div>
-            
-        </div>
-    )
+            const {data: servicePackages } = await api.get('/ListPackages')
+
+            setServicePackage(servicePackages)
+        }
+
+        listOnload()
+
+    }, [])
+
+    return (
+        <>
+            {servicePackage.map(servicePackage => (
+            <div key={servicePackage.id}>
+                <p>ID: {servicePackage.id}</p>
+                <p>Pet: {servicePackage.pet_id}</p>
+                <p>Tipo: {servicePackage.package_type}</p>
+            </div>
+            ))}
+        </>
+        )
 }
 
