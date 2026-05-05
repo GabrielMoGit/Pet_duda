c02e07b23ba98d66105e3235d7c6cac726702b31
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-05-05 13:22:37 -0300

    Adicionato valor no repositorio de pacote, para adicionar valor ao criar o pacote

--- src/repositories/servicePackageRepository.ts
+++ src/repositories/servicePackageRepository.ts
@@ -10,8 +10,8 @@ class ServicePackageRepository {
         this.repository = dataSource.getRepository(servicePackage)
     }
 
-    async createAndSave(pet_id: string, package_type: string,package_done: number, paid: number){
-        const servicePackage = this.repository.create({pet_id, package_type, package_done, paid})
+    async createAndSave(pet_id: string, package_type: string,package_done: number, paid: number, value: string){
+        const servicePackage = this.repository.create({pet_id, package_type, package_done, paid, value})
         return this.repository.save(servicePackage)
     }
 
