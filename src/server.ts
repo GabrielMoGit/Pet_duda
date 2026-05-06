6a2362f307a9a6c03780b99ffe2a72f106f92e70
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-05-06 15:01:28 -0300

    instalado o cron para trabalhar com execução de tarefa em determinada hora, agora todo dia meia noite ele executa e muda os serviços já passados para feito

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import {app} from "../src/app";
 import { connectDatabase } from "./database";
 import dotenv from "dotenv"
 import { resolve } from "path"
+import cron from "node-cron"
+import { ServiceRepository } from "./repositories/serviceRespository";
+
+const serviceRepository = new ServiceRepository()
 
 dotenv.config({
     path: resolve(__dirname, "../../.env")
@@ -9,3 +13,10 @@ dotenv.config({
 
 connectDatabase()
 app.listen(3000, ()=> console.log("Server is running!"))    
+
+cron.schedule('0 0  * * *', () => {
+    serviceRepository.turnDonethepPassedServices()
+    console.log("set services done");
+}, {
+    timezone: "America/Sao_Paulo"
+});
