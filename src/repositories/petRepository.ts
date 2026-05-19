ac2b57c35ae8f525152ee3ba8d2341e7ef8ebc5f
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-05-19 20:34:13 -0300

    Criado o método para retornar os pets existentes

--- src/repositories/petRepository.ts
+++ src/repositories/petRepository.ts
@@ -33,6 +33,16 @@ class PetRepository{
         }
     }
 
+    async listAllExistentPets(){
+        const pets = await this.repository.find()
+
+        if(!pets){
+            return
+        }
+
+        return pets
+    }
+
 }
 
 export {PetRepository}
