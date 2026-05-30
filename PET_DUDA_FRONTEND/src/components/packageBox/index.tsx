4b9e5317cd79db356060b618d9b56222d80485c1
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-05-29 23:56:36 -0300

    Alterada a disposição de data e hora dos serviços no box de pacote, para ficar melhor no mobile

--- PET_DUDA_FRONTEND/src/components/packageBox/index.tsx
+++ PET_DUDA_FRONTEND/src/components/packageBox/index.tsx
@@ -74,26 +74,33 @@ export function StyledBox(props: Props){
 
                 <div style={{backgroundColor: '#fff5e2', borderRadius: '15px', padding: '10px', marginTop: '0px', boxShadow: 'inset 0 2px 4px rgba(0,0,0,0.1)'}}>
 
-                    <p><img src='board.png' width={30} style={{ marginRight: "10px", marginLeft: '23px', marginTop: '5px'}}/>
+                    <p><img src='board.png' width={30} style={{ marginRight: "10px", marginLeft: '5px', marginTop: '5px'}}/>
 
                     <span style={{color: '#ff7b00', fontWeight: 'bold', fontSize: '25px'}}>Serviços:</span></p>
 
                     <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', rowGap: '1px', alignItems: 'center', marginLeft: '5px'}}>
                         {props.services.map(service => (
                             <React.Fragment key={service.service_id}>
-                                <div>
-                                    <img src='orangeCalendar.png' width={25} style={{ marginRight: "8px", marginLeft: '22px' }}/> 
-                                    <span style={{ fontSize: '20px' }}>{new Date(service.service_date).toLocaleString('pt-BR',{
-                                        day: '2-digit',
-                                        month: '2-digit',
-                                        year: '2-digit',
-                                        hour: '2-digit',
-                                        minute: '2-digit'
-                                    }
-                                    )}</span>
+                                
+                                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                    
+                                    <span style={{ fontSize: '22px' }}>
+                                        <img src='orangeCalendar.png' width={25} style={{marginRight: '5px'}}/> 
+                                        {new Date(service.service_date).toLocaleDateString('pt-BR', {
+                                            day: '2-digit',
+                                            month: '2-digit',
+                                            year: '2-digit',
+                                        })}
+                                    </span>
+                                    <span style={{ fontSize: "22px", marginLeft: '15px'}}>
+                                            Horário: {new Date(service.service_date).toLocaleTimeString('pt-BR', {
+                                            hour: '2-digit',
+                                            minute: '2-digit'
+                                        })}
+                                    </span>
                                 </div>
-                                <div>
-                                    <span style={{ fontSize: 'px' }}>Status: </span>
+                                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                    <span style={{ fontSize: '22px' }}>Status: </span>
                                     <span style={{ fontSize: '22px', backgroundColor: service.service_done === 1 ? '#00b60310' :  '#feead7', color: service.service_done === 1 ? '#00b603' : '#ff7b00', padding: '1px 10px', borderRadius: '10px',
                                         marginLeft: '10px', fontWeight: 'bold' }}>{service.service_done ? "Concluído" : "Pendente"}
                                     </span>
@@ -102,7 +109,7 @@ export function StyledBox(props: Props){
                         ))}
                     </div>
                 </div>
-                <div style={{marginTop: '10px', display: 'flex', justifyContent: 'space-between'}}>
+                <div style={{marginTop: '10px', display: 'flex', justifyContent: 'space-between', gap: '15px'}}>
                     <div style={{backgroundColor: '#eff3ff', padding: '1px 10px', borderRadius: '10px', fontSize: '20px', display: 'flex'}}>
                         <div style={{padding: '5px 10px', borderRadius: '10px', display: 'flex', alignItems: 'center', gap: '10px'}}>
                             <div style={{ backgroundColor: '#abc1ff9a', borderRadius: '10px', padding: '5px', display: 'flex',
