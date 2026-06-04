8a0378217a26fe7e9913fdb3451629881fcbc3bb
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-06-04 15:17:40 -0300

    Adicionado ao repositorio de pacote, no metodo de criação o parametro de data de referenai

--- src/repositories/servicePackageRepository.ts
+++ src/repositories/servicePackageRepository.ts
@@ -10,8 +10,8 @@ class ServicePackageRepository {
         this.repository = dataSource.getRepository(servicePackage)
     }
 
-    async createAndSave(pet_id: string, package_type: string,package_done: number, paid: number, value: string, active_package: number){
-        const servicePackage = this.repository.create({pet_id, package_type, package_done, paid, value, active_package})
+    async createAndSave(pet_id: string, package_type: string, reference_date: Date, package_done: number, paid: number, value: string, active_package: number){
+        const servicePackage = this.repository.create({pet_id, package_type, reference_date, package_done, paid, value, active_package})
         return this.repository.save(servicePackage)
     }
 
